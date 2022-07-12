e459c8c68d6fc9436e1249fb14db003bcaec582a
Alexsei <repyls2@mail.ru>
2022-07-12 16:06:33 +0300

    add fileUpload

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <single-file/>
+  <div id="app">
+    <h1>Upload a File</h1>
+    <form enctype="multipart/form-data">
+      <input type="file" name="file" v-on:change="fileChange($event.target.files)" />
+      <button type="button" v-on:click="upload()">Upload</button>
+    </form>
+  </div>
 </template>
 
 <script>
-import SingleFile from './components/SingleFile.vue';
-
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    SingleFile,
+  name: 'app',
+  data() {
+    return {
+      files: new FormData()
+    }
+  },
+  methods: {
+    fileChange(fileList) {
+      this.files.append("file", fileList[0], fileList[0].name);
+    },
+    upload() {
+      axios({ method: "POST", "url": "http://localhost:9000/upload", "data": this.files }).then(result => {
+        console.dir(result.data);
+      }, error => {
+        console.error(error);
+      });
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  max-width: 600px;
-  margin: auto;
 }
 </style>
