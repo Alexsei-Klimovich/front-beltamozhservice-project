662513d291d5e2f7abbc30ee6e3dbc3b2a8d1684
Yaroslav <yarikshtykh@gmail.com>
2022-07-20 12:33:40 +0300

    update upload files method

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,80 @@
+<style>
+input[type="file"]{
+  position: absolute;
+  top: -500px;
+}
+div.file-listing{
+  width: 200px;
+}
+span.remove-file{
+  color: red;
+  cursor: pointer;
+  float: right;
+}
+</style>
 <template>
-  <div id="app">
-    <h1>Upload a File</h1>
-    <form enctype="multipart/form-data">
-      <input type="file" name="file" v-on:change="fileChange($event.target.files)" />
-      <button type="button" v-on:click="upload()">Upload</button>
-    </form>
+  <div class="container" id="app">
+    <div class="large-12 medium-12 small-12 cell">
+      <h1>Upload a File</h1>
+      <h2>Chose "XML" and "XLSX"/"XLS" files</h2>
+      <label>Files
+        <input type="file" id="files" ref="files" multiple v-on:change="handleFilesUpload()"/>
+      </label>
+    </div>
+    <div class="large-12 medium-12 small-12 cell">
+      <div v-for="(file, key) in files" class="file-listing">{{ file.name }} <span class="remove-file" v-on:click="removeFile( key )">Remove</span></div>
+    </div>
+    <br>
+    <div class="large-12 medium-12 small-12 cell">
+      <button v-on:click="addFiles()">Add Files</button>
+    </div>
+    <br>
+    <div class="large-12 medium-12 small-12 cell">
+      <button v-on:click="submitFiles()">Submit</button>
+    </div>
   </div>
 </template>
-
 <script>
-import axios from "axios";
-
 export default {
-  name: 'app',
-  data() {
+  data(){
     return {
-      files: new FormData()
+      files: []
     }
   },
   methods: {
-    fileChange(fileList) {
-      this.files.append("file", fileList[0], fileList[0].name);
+    addFiles(){
+      this.$refs.files.click();
     },
-    upload() {
-      axios({ method: "POST", "url": "http://localhost:9000/upload", "data": this.files }).then(result => {
-        console.dir(result.data);
-      }, error => {
-        console.error(error);
-      });
+    submitFiles(){
+
+      let formData = new FormData();
+      for( var i = 0; i < this.files.length; i++ ){
+        let file = this.files[i];
+        formData.append('files[' + i + ']', file);
+      }
+      axios.post( '/select-files',
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          }
+      ).then(function(){
+        console.log('SUCCESS!!');
+      })
+          .catch(function(){
+            console.log('FAILURE!!');
+          });
+    },
+    handleFilesUpload(){
+      let uploadedFiles = this.$refs.files.files;
+      for( var i = 0; i < uploadedFiles.length; i++ ){
+        this.files.push( uploadedFiles[i] );
+      }
+    },
+    removeFile( key ){
+      this.files.splice( key, 1 );
     }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
